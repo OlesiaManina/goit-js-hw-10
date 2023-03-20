35a30d7e7340206628196c6d8b7a6f5613c1f396
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-03-20 18:55:04 +0200

    Дз перед проверкой

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,9 +3,8 @@ export default function fetchCountries(searchQuery) {
     const queryParams = '?fields=name,capital,population,flags,languages'
     return fetch(`https://restcountries.com/v3.1/name/${searchQuery}${queryParams}`)
     .then((response) => { if (!response.ok) {
-        throw new Error(response.status);
+        Notiflix.Notify.failure("Oops, there is no country with that name");
       }
-      return response.json()}).catch(console.error('Error fetch!!!!'));
- }
+      return response.json()}).catch((error) => console.error(error));
 
-
+    }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import fetchCountries from "./fetchCountries";
+import Notiflix from 'notiflix';
 
 var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
@@ -8,26 +9,36 @@ const inputRef = document.querySelector('input#search-box');
 const listRef = document.querySelector('.country-list');
 const infoRef = document.querySelector('.country-info');
 
-const searchQuery = '';
+let searchQuery = '';
 
 
 inputRef.addEventListener('input', debounce(inputFetch, DEBOUNCE_DELAY));
 
 function inputFetch(searchQuery) {
-    console.log(inputRef.value);
     searchQuery = inputRef.value.trim();
-    fetchCountries(searchQuery).then(countries => renderMarkup(countries)).catch(console.error('Error inputFetch'));
+    fetchCountries(searchQuery)
+    .then((countries) => renderMarkup(countries))
+    .catch(error => { 
+        console.error(error);
+        Notiflix.Notify.failure("Oops, there is no country with that name");
+    });
 
 }
 
 function renderMarkup(countries) {
-    console.log('страны', countries);
-    if (countries.length === 0) {
+  
+    if (countries.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    }
+    listRef.innerHTML = "";
+    infoRef.innerHTML = "";
+
+    if (countries.length < 1) {
             listRef.innerHTML = "";
-            infoRef.innerHTML = "" 
-     } else if(countries.length > 2 && countries.length < 10) {
+            infoRef.innerHTML = "";
+     } else if(countries.length > 1 && countries.length < 10) {
             appendlistMarkup(countries);
-        } else {
+        } else if(countries.length === 1){
             appendInfoMarkup(countries);
         }
 }
@@ -35,28 +46,30 @@ function renderMarkup(countries) {
 
 function appendInfoMarkup(countries) {
 
-    const infoMarkup = countries.map(({ name: { official }, flags: { svg }, capital, population, languages }) => { 
-        const language = Object.values(languages).join(", ");
-        `
-    <h1><img src="${svg}"/><span>${official}</span></h1>
-        <p>Capital: ${capital}</p>
-        <p>Population:  ${population}</p>
-        <p>Languages:  ${language}</p>`; }).join('');
-
         for (const country of countries) {
-            listRef.innerHTML = infoMarkup;
+            const { name: { official }, flags: { svg }, capital, population, languages } = country;
+            const language = Object.values(languages).join(", ");
+
+            const infoMarkup = `
+                <h1 class="country-info__item"><img src="${svg}" width="50" height="30"/><span>${official}</span></h1>
+                    <p>Capital: ${capital}</p>
+                    <p>Population:  ${population}</p>
+                    <p>Languages:  ${language}</p>`; 
+            infoRef.innerHTML = infoMarkup;
         }
 }
 
 
 function appendlistMarkup(countries) {
 
-    const listMarkup = countries.map(({ name: { official }, flags: { svg }}) => { `
-    <li class="country-list__item">
-        <img src="${svg}"/><span>${official}</span>
+    const listMarkup = countries.map((country) => { 
+        const { name: { official }, flags: { svg }} = country;
+        return ` <li class="country-list__item">
+        <img src="${svg}" width="30" height="18"/><span>${official}</span>
       </li>`; }).join('');
 
      listRef.innerHTML = listMarkup;
 
 }
-    
+
+
